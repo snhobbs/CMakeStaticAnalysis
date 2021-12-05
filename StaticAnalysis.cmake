75da8764471aaaf47ebfcca7b97c980bb5dab3f7
simon <simon.hobbs@electrooptical.net>
2021-12-05 12:24:05 -0500

    adding defaults

--- StaticAnalysis.cmake
+++ StaticAnalysis.cmake
@@ -1,3 +1,16 @@
+if(NOT DEFINED CpplintCommand)
+  set(CpplintCommand cpplint)
+endif()
+
+if(NOT DEFINED CppcheckCommand)
+  set(CppcheckCommand cppcheck)
+endif()
+
+if(NOT DEFINED ClangtidyCommand)
+  set(ClangtidyCommand clang-tidy)
+endif()
+
+
 if(NOT DEFINED CMAKE_CXX_CPPLINT)
 set(CMAKE_CXX_CPPLINT "cpplint;--verbose=1;--linelength=100")
 endif()
